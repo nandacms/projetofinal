1a8e2518a11db8cba57035ca6a84acf8db77f880
Treinamento <Treinamento@MW7LV27OB9PEC6.dir.svc.accenture.com>
2016-05-26 18:00:33 -0300

    Modificações de Wanderson

--- public/js/compraController.js
+++ public/js/compraController.js
@@ -1,7 +1,10 @@
 app.controller("compraController", function($scope,$http) {
 	
+	$scope.qntdEscolhida = "";
 	$scope.listaProdutos = [];
 	$scope.produtosEscolhidos = [];
+	$scope.id;
+	$scope.soma = 0;
 	
 	
 	$scope.listarProdutos = function(){
@@ -18,6 +21,43 @@ app.controller("compraController", function($scope,$http) {
 	};
 	$scope.listarProdutos();
 	
+	$scope.adicionarProdutos = function(id, produtos){
+		$scope.produtosEscolhidos.push(produtos);
+		var obj = id;
+		for(var x=0; x < $scope.produtosEscolhidos.length; x++){
+			if($scope.produtosEscolhidos[x].qntdEscolhida <= $scope.produtosEscolhidos[x].quantidadeEstoque){
+				if($scope.produtosEscolhidos[x]._id == obj){
+					$scope.produtosEscolhidos[x].valorTotal = $scope.produtosEscolhidos[x].precoProduto * $scope.produtosEscolhidos[x].qntdEscolhida;
+					$scope.soma = $scope.soma + $scope.produtosEscolhidos[x].valorTotal;
+				}
+			}else{
+				$scope.produtosEscolhidos.splice(x,1);
+				alert("Quantidade escolhida maior do que a quantidade em estoque.");
+			}
+		}
+	};
 	
+	$scope.adicionarCompra = function(){
+		var listacompras = Object();
+		listacompras._id = "5746feb4d78b68fc02ee28dc";
+		listacompras.compra = new Object();
+		listacompras.compra.valorCompra = $scope.soma;
+		listacompras.compra.status = "comprado";
+		listacompras.compra.produtos = $scope.produtosEscolhidos;
+		$http.put("/adicionarCompra", listacompras, {
+			headers: {'Content-Type': 'application/json'}
+		})
+		.then(
+			function(response){
+				/*$scope.listarContato();
+				$scope.limpar();
+				$scope.listaTelefones = response.data;*/
+				
+			},
+			function(response){
+				alert(response.data);
+			}
+		);
+	};
 	
 });
