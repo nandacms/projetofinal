f4b9bd95e001cbd1d43abb60377de6f2e05b4f23
Treinamento <Treinamento@MW7LV27OB9PEC6.dir.svc.accenture.com>
2016-05-26 17:23:41 -0300

    Controller parcial de Venda

--- public/js/vendaController.js
+++ public/js/vendaController.js
@@ -1,4 +1,66 @@
 app.controller("vendaController", function($scope, $http) {
 	
+	$scope.listaProdutos = [];
+	$scope.produtosEscolhidos = [];
+	
+	
+	/*$scope.descricao = "";
+	$scope.precoProduto = 0 ;
+	$scope.quantidadeEstoque = 0;*/
+	$scope.valorVenda = 0;
+	
+	
+	$scope.listarProdutos = function(){
+		$http({
+			method:'GET',
+			url:'/getProdutos'
+		})
+		.then(function successCallback(response){
+			$scope.listaProdutos = response.data;
+		},
+		function errorCallback(response){
+			alert(response.data);
+		});
+	};
+	
+	$scope.listarProdutos();
+	
+	$scope.adicionarProdutosEscolhidos = function (produtos){
+		var produto ={ descricao: produtos.descricao,
+				precoProduto:produtos.precoProduto,
+				quantidadeEstoque: produtos.quantidadeEstoque
+		}
+		
+		$scope.valorVenda = produtos.precoProduto;
+		$scope.produtosEscolhidos.push(produtos);
+		
+		
+		
+	}
+	
+	
+	$scope.deletarProdutoEscolhido = function(index){
+		$scope.produtosEscolhidos.splice(index,1);
+	}
+	
+	$scope.adicionarVenda = function (){
+		
+	}
+	
+/*$scope.inserirEmpresa= function(){
+		
+		Para que pegue as propriedades que já estão na tela em relação ao interesse
+			
+		var empresa = {nomeFantasia: $scope.nomeFantasia, 
+						razaoSocial: $scope.razaoSocial, 
+						cnpj: $scope.cnpj, 
+						cidade: $scope.cidade };
+						
+		$scope.empresas.push(empresa);
+		
+		$scope.limpar();
+		
+	}*/
+	
 	
 });
